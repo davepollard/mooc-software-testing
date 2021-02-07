ee2e6c4825e71ad3a4c1f5afc69daed9f0dfb174
d2-pollard <david2.pollard@live.uwe.ac.uk>
2021-02-07 16:22:21 +0100

    next exercise

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,27 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
 
+    @Test
+    void applyDiscount(){
+        Product p1 = new Product("p1", 10, "HOME");
+        Product p2 = new Product("p2", 100, "BUSINESS");
+        List<Product> results = Arrays.asList(p1, p2);
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier da = new DiscountApplier(dao);
+        da.setNewPrices();
+
+        Assertions.assertEquals(9, p1.getPrice());
+        Assertions.assertEquals(110, p2.getPrice());
+    }
 }
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,69 @@
 package tudelft.sum;
 
-public class TwoNumbersSumTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+public class TwoNumbersSumTest {
+    @Test
+    void addTwoNoComplement(){
+        ArrayList a1 = new ArrayList();
+        a1.add(3);
+        a1.add(1);
+
+        ArrayList a2 = new ArrayList();
+        a2.add(6);
+        a2.add(2);
+
+        ArrayList result = new ArrayList();
+        ArrayList expectResult = new ArrayList();
+        expectResult.add(9);
+        expectResult.add(3);
+        TwoNumbersSum tns = new TwoNumbersSum();
+        result = tns.addTwoNumbers(a1, a2);
+
+        Assertions.assertEquals(expectResult, result);
+    }
+
+    @Test
+    void addTwoOneComplement(){
+        ArrayList a1 = new ArrayList();
+        a1.add(3);
+        a1.add(5);
+
+        ArrayList a2 = new ArrayList();
+        a2.add(4);
+        a2.add(7);
+
+        ArrayList result = new ArrayList();
+        ArrayList expectResult = new ArrayList();
+        expectResult.add(8);
+        expectResult.add(2);
+        TwoNumbersSum tns = new TwoNumbersSum();
+        result = tns.addTwoNumbers(a1, a2);
+
+        Assertions.assertEquals(expectResult, result);
+    }
+
+    @Test
+    void addTwoExtraDigit(){
+        ArrayList a1 = new ArrayList();
+        a1.add(7);
+        a1.add(5);
+
+        ArrayList a2 = new ArrayList();
+        a2.add(4);
+        a2.add(7);
+
+        ArrayList result = new ArrayList();
+        ArrayList expectResult = new ArrayList();
+        expectResult.add(1);
+        expectResult.add(2);
+        expectResult.add(2);
+        TwoNumbersSum tns = new TwoNumbersSum();
+        result = tns.addTwoNumbers(a1, a2);
+
+        Assertions.assertEquals(expectResult, result);
+    }
 }
